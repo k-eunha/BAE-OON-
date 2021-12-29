d06622b9ad34d15e01fec7be538c271e6e237057
k-eunha <96608321+k-eunha@users.noreply.github.com>
2021-12-30 00:41:04 +0900

    Add files via upload

---
+++ LEVEL_3/src/n2742/Main.java
@@ -0,0 +1,16 @@
+package n2742;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		for(int i=num; 0<i; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
