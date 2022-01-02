ab86f7a4166cd3cdf8f744a415626d44d22a3a81
k-eunha <96608321+k-eunha@users.noreply.github.com>
2022-01-02 21:56:40 +0900

    Add files via upload

---
+++ LEVEL_4/src/n10951/Main.java
@@ -0,0 +1,19 @@
+package n10951;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int num1;
+		int num2;
+		while(true) {
+			num1 = scan.nextInt();
+			num2 = scan.nextInt();
+			System.out.println(num1+num2);
+		}		
+	}
+
+}
