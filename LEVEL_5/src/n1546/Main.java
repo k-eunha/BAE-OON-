4f9b3e7124a6df3451e12bf2342bbde3fc3c2adc
k-eunha <96608321+k-eunha@users.noreply.github.com>
2022-01-11 09:40:06 +0900

    Add files via upload

---
+++ LEVEL_5/src/n1546/Main.java
@@ -0,0 +1,24 @@
+package n1546;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int [] number = new int[sc.nextInt()];
+		int max =0;
+		double sum=0.0;
+		for (int i=0; i<number.length; i++) {
+			number[i]=sc.nextInt();
+			if(max<number[i])
+				max=number[i];
+		}
+		for (int i=0; i<number.length;i++) {
+			sum += ((double)number[i] /(double)max)*100;
+		}
+		System.out.println(sum/number.length);
+	}
+}
